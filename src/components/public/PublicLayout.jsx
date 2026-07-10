91163d8cc21c8462a943b32da406f1e30442bdc0
masinlocandher-max <masinlocandher@gmail.com>
2026-07-11 00:29:31 +0800

    Use corporate shell with futuristic page transitions

--- src/components/public/PublicLayout.jsx
+++ src/components/public/PublicLayout.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import PublicNav from "@/components/public/PublicNav";
 import PublicFooter from "@/components/public/PublicFooter";
+import RouteTransition from "@/components/RouteTransition";
 
 export default function PublicLayout() {
   return (
-    <div className="universe-shell min-h-screen flex flex-col">
+    <div className="corporate-shell min-h-screen flex flex-col">
       <PublicNav />
       <main className="flex-1 pt-16">
-        <Outlet />
+        <RouteTransition>
+          <Outlet />
+        </RouteTransition>
       </main>
       <PublicFooter />
     </div>
