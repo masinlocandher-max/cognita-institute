67fdf7a32b852690b759527175ff7f55fb57a3da
masinlocandher-max <masinlocandher@gmail.com>
2026-07-11 00:21:06 +0800

    Apply universe shell to public interface

--- src/components/public/PublicLayout.jsx
+++ src/components/public/PublicLayout.jsx
@@ -5,7 +5,7 @@ import PublicFooter from "@/components/public/PublicFooter";
 
 export default function PublicLayout() {
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="universe-shell min-h-screen flex flex-col">
       <PublicNav />
       <main className="flex-1 pt-16">
         <Outlet />
